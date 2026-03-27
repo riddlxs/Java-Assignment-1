41077e903edfff23402d018b433becf3d91fafe1
Liana <lianahockin@gmail.com>
2026-03-27 16:58:41 -0400

    changed colour to color per professor's recording

--- animal.java
+++ animal.java
@@ -2,15 +2,15 @@ public class Animal { // create the animal class and make it public so it can be
 
     private String name; // create the strings and int (for numbered age) for the animal class to run
                          // with the zoo class
-    private String colour;
+    private String color;
     private int age;
     private String favFood;
 
-    public Animal(String name, String colour, int age, String favFood) {
+    public Animal(String name, String color, int age, String favFood) {
         this.name = name; // use this. since professor said its important to know the exact variable being
                           // used
-        this.colour = colour; // this variable colour is the same one being used in the zoo class so this. is
-                              // needed
+        this.color = color; // this variable color is the same one being used in the zoo class so this. is
+                            // needed
         this.age = age;
         this.favFood = favFood;
     }
@@ -20,11 +20,11 @@ public class Animal { // create the animal class and make it public so it can be
 
     public String getName() { // create the getters for the animal class to run with the zoo class. this file
                               // this file holds data, zoo file prints the data from users input
-        return name; // get the name (data) inputted and return it to the zoo class.
+        return name; // get the name inputted and return it to the zoo class.
     }
 
-    public String getColour() { // same as above, get the colour (data) inputted and return it to the zoo class
-        return colour;
+    public String getcolor() { // same as above, get the color (data) inputted and return it to the zoo class
+        return color;
     }
 
     public int getAge() {
@@ -43,8 +43,8 @@ public class Animal { // create the animal class and make it public so it can be
         this.name = name;
     }
 
-    public void setColour(String colour) {
-        this.colour = colour;
+    public void setcolor(String color) {
+        this.color = color;
     }
 
     public void setAge(int age) {
@@ -61,7 +61,7 @@ public class Animal { // create the animal class and make it public so it can be
                                  // of the animal
         System.out.printf("Animal Details:\n"); // /n means new line (makes it look cleaner)
         System.out.printf("Name: %s\n", name); // %s is used for strings
-        System.out.printf("Colour: %s\n", colour);
+        System.out.printf("color: %s\n", color);
         System.out.printf("Age: %d\n", age); // %d is used for integers since the age being inputted is a number, not
                                              // string
         System.out.printf("Favorite Food: %s\n", favFood);
